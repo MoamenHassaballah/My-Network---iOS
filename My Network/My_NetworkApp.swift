c051c21fc30ce17133fe3f5bedb4c46e1f249d34
Moamen Hassaballah <hassaballahmoamen84@gmail.com>
2024-04-06 00:24:58 +0200

    Initial Commit

---
+++ My Network/My_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_NetworkApp.swift
+//  My Network
+//
+//  Created by Moamen Hassaballah on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct My_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
